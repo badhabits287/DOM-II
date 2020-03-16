c3d3862ea05284c2a27f4c3a3971b1c5bb4d68ec
Javo <gutierrezsanchez287@gmail.com>
2020-03-16 12:15:50 -0400

    runOnKeys function to load

--- js/index.js
+++ js/index.js
@@ -2,136 +2,144 @@
 
 
 
-//on Keys 'L' & 'A'  load Page Code 
+//on Keys 'F,U,N' load Page Code 
 
 function runOnKeys(func, ...codes) {
     let pressed = new Set();
 
-    document.addEventListener('keydown', function(event) {
-      pressed.add(event.code);
+    document.addEventListener('keydown', function (event) {
+        pressed.add(event.code);
 
-      for (let code of codes) { 
-        if (!pressed.has(code)) {
-          return;
+        for (let code of codes) {
+            if (!pressed.has(code)) {
+                return;
+            }
         }
-      }
 
-      pressed.clear();
+        pressed.clear();
 
-      func();
+        func();
     });
 
-    document.addEventListener('keyup', function(event) {
-      pressed.delete(event.code);
+    document.addEventListener('keyup', function (event) {
+        pressed.delete(event.code);
     });
 
-  }
+}
 
-  runOnKeys(
-    () => ShowPage (console.log('welcome to the FunBus !'))
+runOnKeys(
+    () => ShowPage(console.log('welcome to the FunBus !'))
     ,
-    "KeyL",
-    "KeyA",
+    "KeyF",
+    "KeyU",
+    "KeyN",
     
-    
-  );
 
 
-  function ShowPage () {
-
-//window
-window.addEventListener('load', (event) => {
+);
 
 
-    console.log('Eventlistener Load' + 'The Bus Is Loaded with Fun!');
-});
+function ShowPage() {
 
-//on scroll background changue 
-
-const [red, green, blue] = [69, 111, 225]
-const section1 = document.querySelector('body')
-
-window.addEventListener('scroll', () => {
-  let y = 1 + (window.scrollY || window.pageYOffset) / 150
-  y = y < 1 ? 1 : y     // ensure y is always >= 1 (due to Safari's elastic scroll)
-  const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
-  section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-})
+    //window
+    window.addEventListener('load', (event) => {
 
 
-// // background if Function @media 
+        console.log('Eventlistener Load' + 'The Bus Is Loaded with Fun!');
+    });
 
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-        document.querySelector("body > header").style.backgroundColor = "dodgerBlue";
-        document.querySelector("body > header > div > h1").style.marginLeft = 'unset'
-    } else {
+    //on scroll background changue 
 
-        document.querySelector("body > header").style.backgroundColor = "lightBlue";
-        document.querySelector("body > header > div > h1").style.marginLeft = '140px'
+    const [red, green, blue] = [69, 111, 225]
+    const section1 = document.querySelector('body')
+
+    window.addEventListener('scroll', () => {
+        let y = 1 + (window.scrollY || window.pageYOffset) / 150
+        y = y < 1 ? 1 : y     // ensure y is always >= 1 (due to Safari's elastic scroll)
+        const [r, g, b] = [red / y, green / y, blue / y].map(Math.round)
+        section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    })
+
+
+    // // background if Function @media 
+
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            document.querySelector("body > header").style.backgroundColor = "dodgerBlue";
+            document.querySelector("body > header > div > h1").style.marginLeft = 'unset'
+        } else {
+
+            document.querySelector("body > header").style.backgroundColor = "lightBlue";
+            document.querySelector("body > header > div > h1").style.marginLeft = '140px'
+
+        }
+
 
     }
+    var x = window.matchMedia("(max-width: 700px)")
+    myFunction(x) // Call listener function  'run time'
+    x.addListener(myFunction)// Attach listener function  'state change'
 
 
-}
-var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function  'run time'
-x.addListener(myFunction)// Attach listener function  'state change'
+    // nav 
+    const navClk = document.querySelectorAll('a')
 
 
-// nav 
-const navClk = document.querySelectorAll('a')
+    navClk.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.target.style.color = 'red';
+        });
+        link.addEventListener('mouseover', (event) => {
+            event.target.style.transform = "scale(2.2)";
+            event.target.style.transition = "all 0.3s";// smoth transition effect 
+            event.target.style.color = 'yellow'
+        });
+        link.addEventListener('mouseleave', (event) => {
+            event.target.style.transform = "scale(1)"
+            event.target.style.transition = "all 0.3s";// smoth transition effect 
 
+        });
 
-navClk.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.target.style.color = 'red';
     });
-    link.addEventListener('mouseover', (event) => {
-        event.target.style.transform = "scale(2.2)";
-        event.target.style.transition = "all 0.3s";// smoth transition effect 
-        event.target.style.color = 'yellow'
-    });
-    link.addEventListener('mouseleave', (event) => {
-        event.target.style.transform = "scale(1)"
-        event.target.style.transition = "all 0.3s";// smoth transition effect 
+    //body background 
 
-    });
+    function myFunction2(x) {
+        if (x.matches) { // If media query matches
+            document.querySelector("body ").style.backgroundColor = "lightBlue";
 
-});
-//body background 
+        } else {
 
-function myFunction2(x) {
-    if (x.matches) { // If media query matches
-        document.querySelector("body ").style.backgroundColor = "lightBlue";
+            document.querySelector("body").style.backgroundColor = "dodgerBlue";
 
-    } else {
+        }
 
-        document.querySelector("body").style.backgroundColor = "dodgerBlue";
 
     }
+    var x = window.matchMedia("(max-width: 700px)")
+    myFunction2(x) // Call listener function  'run time'
+    x.addListener(myFunction2)// Attach listener function  'state changes'
 
 
-}
-var x = window.matchMedia("(max-width: 700px)")
-myFunction2(x) // Call listener function  'run time'
-x.addListener(myFunction2)// Attach listener function  'state changes'
+    // bus img
+
+    const busImg = document.querySelector("header img")
+    busImg.addEventListener('dblclick', (event) => {
+        event.target.style.border = '3px solid orange'
+        event.target.style.borderRadius = '20px'
+
+    });
 
 
-// bus img
+    //footer appendChild 
 
-const busImg = document.querySelector("header img")
-busImg.addEventListener('dblclick', (event) => {
-    event.target.style.border = '3px solid orange'
-    event.target.style.borderRadius = '20px'
+    const newAT = document.createElement("H2");                 // Create <> node
+    var textnode = document.createTextNode("Lambda");          // Create a 'method' node
+    newAT.appendChild(textnode);                               // Append the text to <>
+    document.querySelector('footer p').appendChild(newAT);          //  Append child 
 
-});
+    // btns 
 
 
-//footer appendChild 
 
-const newAT = document.createElement("H2");                 // Create <> node
-var textnode = document.createTextNode("Lambda");          // Create a 'method' node
-newAT.appendChild(textnode);                               // Append the text to <>
-document.querySelector('footer p').appendChild(newAT);          //  Append child 
-}
+
+};
